d0558e99ca0d6bc06bf89e8dc09254031b15598e
Pavel Shevnin <pshevnin49@gmail.com>
2022-07-11 12:35:27 +0200

    Login page

---
+++ View/login.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="../style/style.css">
+        <link href="https: //fonts.googleapis.com/css2?family= Roboto:ital, wght @1,100 & display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    </head>
+
+
+    <body>
+        <form class="login container mt-4 " id="login_form" role="form" method="post" action="">
+
+            <div class="row-padded">
+
+                <h1 id="login_header" >Login</h1>
+                <input type="email" class="form-control" name="email" id="email" value="" placeholder="Set email" required></label>
+                <br>
+                <input type="password" class="form-control" name="password" id="password" value="" placeholder="Set password" required></label>
+                <br>
+
+                <label>New to portal? Sign up</label>
+                <br>
+                <button class="btn" id="button_login" name="action" value="login" type="submit" >Sign in</button><br>
+
+
+            </div>
+
+        </form>
+
+    </body>
+
+</html>
+
