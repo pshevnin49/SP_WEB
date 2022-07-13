9212849073f70683c5b96bf62d30c3c849161cad
Pavel Shevnin <pshevnin49@gmail.com>
2022-07-13 13:36:43 +0200

    No rights page but dosnt work

---
+++ View/no_rights.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../style/style.css">
+    <link href="https: //fonts.googleapis.com/css2?family= Roboto:ital, wght @1,100 & display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
+
+
+<body>
+<form class="login container mt-4 " id="no_rights_form" role="form" method="post" action="">
+
+    <label id="no_rights_label">You do not have rights to access this page</label>
+
+</form>
+
+</body>
+
+</html>
+
